2ca2e4cc4563aeec519a8f3710ecccaa83c10a4c
Suguru Tokuda <sugurut@wolfram.com>
2020-01-29 13:49:23 -0600

    Added navigation stack

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation';
+import Login from './secreens/Login';
+import HomeTabNavigation from './screens/Home/tabNavigation';
+import ChatList from './screens/ChatList';
+import ChatScreen from './screens/ChatScreen';
+import UserDetails from './screens/UserDetails';
+import Settings from './screens/Settings';
+import EditProfile from './screens/EditProfile/editProfile';
+import CurrentWork from './screens/EditProfile/currentWork';
+import AddPhoto from './screens/EditProfile/addPhoto';
 
 const App = createStackNavigator(
     {
@@ -9,6 +18,7 @@ const App = createStackNavigator(
         UserDetails: {screen: UserDetails},
         Settings: {screen: Settings},
         EditProfile: {screen: EditProfile},
+        AddPhoto: {screen: AddPhoto},
         CurrentWork: {screen: CurrentWork},
         PhotoCardDetails: {screen: PhotoCardDetails}
     },
