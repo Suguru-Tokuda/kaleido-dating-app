e75c5b77de73139a9124d34855df870c04e88bac
Suguru Tokuda <sugurut@wolfram.com>
2020-03-03 11:12:53 -0600

    Created a basic structure of the app

--- App.js
+++ App.js
@@ -1,106 +1,65 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import Login from './src/screens/login';
+import Swipe from './src/screens/swipe';
+import Messages from './src/screens/messages';
+import Search from './src/screens/search';
+import Settings from './src/screens/settings';
+import store from './src/store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
 
-const App: () => React$Node = () => {
+
+function Home() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One:</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let icon;
+          if (route.name === 'Swipe') {
+            icon = <MaterialIcon name='cards-outline' size={size} color={color} />;
+          } else if (route.name === 'Search') {
+            icon = <FeatherIcon name='search' size={size} color={color} />;
+          } else if (route.name === 'Messages') {
+            icon = <FeatherIcon name='message-circle' size={size} color={color} />;
+          } else if (route.name === 'Settings') {
+            icon = <FeatherIcon name='settings' size={size} color={color} />;
+          }
+          return icon;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'purple',
+        inactiveTintColor: 'gray'
+      }}
+    >
+      <Tabs.Screen name="Swipe" component={Swipe} />
+      <Tabs.Screen name="Search" component={Search} />
+      <Tabs.Screen name="Messages" component={Messages} />
+      <Tabs.Screen name="Settings" component={Settings} />
+    </Tabs.Navigator>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login}></Stack.Screen>
+            <Stack.Screen name="Home" component={Home}></Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    )
+  }
+};
 
 export default App;
